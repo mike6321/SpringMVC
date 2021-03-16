04f1e0d9f5fc241150fb0cbcf7c35d2dbe4925b9
junwoo <33277588+mike6321@users.noreply.github.com>
2021-03-16 14:43:39 +0900

    HTTP 요청 데이터 - API 메세지 바디 - 단순 텍스트

---
+++ servlet/src/main/java/me/choi/servlet/request/RequestBodyStringServlet.java
@@ -0,0 +1,34 @@
+package me.choi.servlet.request;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Project : servlet
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 2:37 오후
+ */
+@WebServlet(name = "RequestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO: HTTP 요청 데이터 - API 메세지 바디 - 단순 텍스트 2021/03/16 2:43 오후
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("ok");
+    }
+}

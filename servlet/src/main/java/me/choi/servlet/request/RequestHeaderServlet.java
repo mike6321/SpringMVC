1453e2334b35a0d22d6a320b33eda6a8458b4a0f
junwoo <33277588+mike6321@users.noreply.github.com>
2021-03-16 13:09:56 +0900

    edit package

--- servlet/src/main/java/me/choi/servlet/request/RequestHeaderServlet.java
+++ servlet/src/main/java/me/choi/servlet/request/RequestHeaderServlet.java
@@ -1,4 +1,4 @@
-package me.choi.servlet;
+package me.choi.servlet.request;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -20,7 +20,7 @@ import java.util.Enumeration;
 public class RequestHeaderServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // TODO: HttpServletRequest 기본사용법 2021/03/16 1:08 오후
+        // TODO: HttpServletRequest 기본사용법 2021/03/16 1:09 오후
         printStartLine(request);
         printHeader(request);
         printHeaderUtils(request);
---
+++ servlet/src/main/java/me/choi/servlet/request/RequestParamServlet.java
@@ -0,0 +1,11 @@
+package me.choi.servlet.request;
+
+/**
+ * Project : servlet
+ *
+ * @author : jwdeveloper
+ * @comment :
+ * Time : 1:09 오후
+ */
+public class RequestParamServlet {
+}
